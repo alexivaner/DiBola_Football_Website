dcb0aec02a3c5e9d30c76bef90f81f74d5170b9e
Ivan Surya Hutomo <hutomoivan@gmail.com>
2020-11-13 17:18:26 +0800

    Add workbox and fix bug on button save and delete

--- js/teams.js
+++ js/teams.js
@@ -10,114 +10,8 @@ const getTeamById = () => {
       caches.match(base_uri + "/teams/" + idParam).then(function(response) {
         if (response) {
           response.json().then(function(data) {
-            let squads = "";
-            data.squad.forEach(function(squad) {
-              shirtnum = squad.shirtNumber || "-";
-              borndate = new Date(squad.dateOfBirth);
-              if (borndate.getMonth() == 0) {
-                month = "No Month";
-              } else {
-                month = borndate.getMonth();
-              }
-              position = squad.position || "-"
-              squads += `
-
-                  <tr>
-                      <td>${position}</td>
-                      <td>${squad.name}</td>
-                      <td>${shirtnum}</td>
-                      <td>${squad.nationality}</td>
-                      <td>${borndate.getFullYear()+"-"+month+"-"+borndate.getDate()}</td>
-                  </tr>
-                  `;
-            });
-
             // Menyusun komponen card artikel secara dinamis
-            var articleHTML = `
-            <div class="container" id="content">
-              <div class="collection center white-text orange darken-1">
-                <h5>Team Details</h5>
-              </div>
-              <div class="card mt-2 p2">
-                <div class="row">
-                  <div class="col s12 m4 center">
-                    <img src="https://crests.football-data.org/${data.id}.svg" alt="${data.name}" class="responsive-img">
-                  </div>
-                  <div class="col s12 m8">
-                    <table class="striped highlight">
-                      <tbody>
-                        <tr>
-                          <th>Team Name</th>
-                          <td>${data.name}</td>
-                        </tr>
-                        <tr>
-                          <th>Short Name</th>
-                          <td>${data.shortName}</td>
-                        </tr>
-                        <tr>
-                          <th>Colors</th>
-                          <td>${data.clubColors}</td>
-                        </tr>
-                        <tr>
-                          <th>Stadium</th>
-                          <td>${data.venue}</td>
-                        </tr>
-                        <tr>
-                          <th>Email</th>
-                          <td>${data.email}</td>
-                        </tr>
-                        <tr>
-                          <th>Country</th>
-                          <td>${data.area.name}</td>
-                        </tr>
-                        <tr>
-                          <th>Founded</th>
-                          <td>${data.email}</td>
-                        </tr>
-                        <tr>
-                          <th>Address</th>
-                          <td>${data.address}</td>
-                        </tr>
-                        <tr>
-                          <th>Phone</th>
-                          <td>${data.phone}</td>
-                        </tr>
-                        <tr>
-                          <th>Website</th>
-                          <td><a target="_blank" href="${data.website}">${data.website}</a> </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-
-                  <div class="collection center white-text orange darken-1">
-                    <h5>Squads</h5>
-                  </div>
-                  <div>
-                    <table class="striped highlight responsive-table">
-                      <thead>
-                        <tr>
-                          <th>Position</th>
-                          <th>Name</th>
-                          <th>Shirt Number</th>
-                          <th>Nationality</th>
-                          <th>Date of Birth</th>
-                        </tr>
-                      </thead>
-                      <tbody id="squadContainer">
-                        ${squads}
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-              <div class="fixed-action-btn">
-                <a class="btn-floating btn-large orange tooltipped" data-position="bottom" data-tooltip="Save this as favorite team!" id="save">
-                  <i class="large material-icons">save</i>
-                </a>
-              </div>
-            </div>
-            `;
+            var articleHTML = getTeamArticle(data);
             // Sisipkan komponen card ke dalam elemen dengan id #content
             document.getElementById("body-content").innerHTML = articleHTML;
             loader.style.display = 'none'
@@ -130,112 +24,8 @@ const getTeamById = () => {
     }
 
     fetchAPI(base_uri + "/teams/" + idParam)
-      .then(function(data) {
-        let squads = "";
-        data.squad.forEach(function(squad) {
-          shirtnum = squad.shirtNumber || "-";
-          borndate = new Date(squad.dateOfBirth);
-          if (borndate.getMonth() == 0) {
-            month = "No Month";
-          } else {
-            month = borndate.getMonth();
-          }
-          position = squad.position || "-"
-          squads += `
-
-              <tr>
-                  <td>${position}</td>
-                  <td>${squad.name}</td>
-                  <td>${shirtnum}</td>
-                  <td>${squad.nationality}</td>
-                  <td>${borndate.getFullYear()+"-"+month+"-"+borndate.getDate()}</td>
-              </tr>
-              `;
-        });
-
-        // Menyusun komponen card artikel secara dinamis
-        var articleHTML = `
-        <div class="container" id="content">
-          <div class="collection center white-text orange darken-1">
-            <h5>Team Details</h5>
-          </div>
-          <div class="card mt-2 p2">
-            <div class="row">
-              <div class="col s12 m4 center">
-                <img src="https://crests.football-data.org/${data.id}.svg" alt="${data.name}" class="responsive-img">
-              </div>
-              <div class="col s12 m8">
-                <table class="striped highlight">
-                  <tbody>
-                    <tr>
-                      <th>Team Name</th>
-                      <td>${data.name}</td>
-                    </tr>
-                    <tr>
-                      <th>Short Name</th>
-                      <td>${data.shortName}</td>
-                    </tr>
-                    <tr>
-                      <th>Colors</th>
-                      <td>${data.clubColors}</td>
-                    </tr>
-                    <tr>
-                      <th>Stadium</th>
-                      <td>${data.venue}</td>
-                    </tr>
-                    <tr>
-                      <th>Email</th>
-                      <td>${data.email}</td>
-                    </tr>
-                    <tr>
-                      <th>Country</th>
-                      <td>${data.area.name}</td>
-                    </tr>
-                    <tr>
-                      <th>Founded</th>
-                      <td>${data.email}</td>
-                    </tr>
-                    <tr>
-                      <th>Address</th>
-                      <td>${data.address}</td>
-                    </tr>
-                    <tr>
-                      <th>Phone</th>
-                      <td>${data.phone}</td>
-                    </tr>
-                    <tr>
-                      <th>Website</th>
-                      <td><a target="_blank" href="${data.website}">${data.website}</a> </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div class="collection center white-text orange darken-1">
-                <h5>Squads</h5>
-              </div>
-              <div>
-                <table class="striped highlight responsive-table">
-                  <thead>
-                    <tr>
-                      <th>Position</th>
-                      <th>Name</th>
-                      <th>Shirt Number</th>
-                      <th>Nationality</th>
-                      <th>Date of Birth</th>
-                    </tr>
-                  </thead>
-                  <tbody id="squadContainer">
-                    ${squads}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        `;
+      .then(function(data) { // Menyusun komponen card artikel secara dinamis
+        var articleHTML = getTeamArticle(data);
         // Sisipkan komponen card ke dalam elemen dengan id #content
         document.getElementById("body-content").innerHTML = articleHTML;
         loader.style.display = 'none'
@@ -252,79 +42,196 @@ const getSavedTeams = () => {
     // Menyusun komponen card artikel secara dinamis
     var articlesHTML = "";
     if (isObjectEmpty(teams)) {
-      articlesHTML=`
-      <div id="error-page">
-
-          <div class="row">
-            <div class="col s12">
-              <div class="browser-window">
-                <div class="top-bar">
-                  <div class="circles">
-                    <div id="close-circle" class="circle"></div>
-                    <div id="minimize-circle" class="circle"></div>
-                    <div id="maximize-circle" class="circle"></div>
-                  </div>
-                </div>
-                <div class="content">
-                    <div id="site-layout-example-right" class="col s12 m12 l12">
-                      <div class="row center">
-                        <h1 class="text-long-shadow col s12">404</h1>
-                        <h5 class="text-long-shadow col s12">Page found but team not found! Add your favorite team!</h5>
-
-                      </div>
-                      <div class="row center">
-                        <p class="center white-text col s12">It seems that this page doesn’t exist.</p>
-                        <p class="center s12">
-                        <a href="index.html" class="btn waves-effect waves-light">Homepage</a>
-                          </p><p>
-                          </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `
+      articlesHTML = getTeamSavedEmpty();
     } else {
-      teams.forEach(function(data) {
-        articlesHTML += `
-        <div class="card">
-          <div class="card-content">
-            <div class="row">
-              <div class="col s12 m4 center">
-                <img src="https://crests.football-data.org/${data.id}.svg" alt="${data.name}" class="responsive-img">
-              </div>
-              <div class="col s12 m8">
-              <h5 class="text-long-shadow col s12"><a href="./team.html?id=${data.id}&saved=true">${data.name}</a></h5>
-                <table class="striped highlight">
-                  <tbody>
-                    <tr>
-                      <th>Team Name</th>
-                      <td>${data.name}</td>
-                    </tr>
-                    <tr>
-                      <th>Short Name</th>
-                      <td>${data.shortName}</td>
-                    </tr>
-                    <tr>
-                      <th>Country</th>
-                      <td>${data.area.name}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-        `;
-      });
+      articlesHTML = getTeamSavedArticle(teams);
     }
-
-
-
     // Sisipkan komponen card ke dalam elemen dengan id #body-content
     document.getElementById("bodyMatches").innerHTML = articlesHTML;
   });
+}
+
+const getTeamSavedEmpty = () => {
+  articlesHTML = `
+  <div id="error-page">
+
+      <div class="row">
+        <div class="col s12">
+          <div class="browser-window">
+            <div class="top-bar">
+              <div class="circles">
+                <div id="close-circle" class="circle"></div>
+                <div id="minimize-circle" class="circle"></div>
+                <div id="maximize-circle" class="circle"></div>
+              </div>
+            </div>
+            <div class="content">
+                <div id="site-layout-example-right" class="col s12 m12 l12">
+                  <div class="row center">
+                    <h1 class="text-long-shadow col s12">404</h1>
+                    <h5 class="text-long-shadow col s12">Page found but team not found! Add your favorite team!</h5>
+
+                  </div>
+                  <div class="row center">
+                    <p class="center white-text col s12">It seems that this page doesn’t exist.</p>
+                    <p class="center s12">
+                    <a href="index.html" class="btn waves-effect waves-light">Homepage</a>
+                      </p><p>
+                      </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  return articlesHTML;
+}
+
+
+const getTeamSavedArticle = (teams) => {
+  var articlesHTML = "";
+  teams.forEach(function(data) {
+    articlesHTML += `
+  <div class="card">
+    <div class="card-content">
+      <div class="row">
+        <div class="col s12 m4 center">
+          <img src="https://crests.football-data.org/${data.id}.svg" alt="${data.name}" class="responsive-img">
+        </div>
+        <div class="col s12 m8">
+        <h5 class="text-long-shadow col s12"><a href="./team.html?id=${data.id}&saved=true">${data.name}</a></h5>
+          <table class="striped highlight">
+            <tbody>
+              <tr>
+                <th>Team Name</th>
+                <td>${data.name}</td>
+              </tr>
+              <tr>
+                <th>Short Name</th>
+                <td>${data.shortName}</td>
+              </tr>
+              <tr>
+                <th>Country</th>
+                <td>${data.area.name}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  });
+  return articlesHTML;
+}
+
+const getTeamArticle = (data) => {
+  let squads = "";
+  data.squad.forEach(function(squad) {
+    shirtnum = squad.shirtNumber || "-";
+    borndate = new Date(squad.dateOfBirth);
+    if (borndate.getMonth() == 0) {
+      month = "No Month";
+    } else {
+      month = borndate.getMonth();
+    }
+    position = squad.position || "-"
+    squads += `
+
+        <tr>
+            <td>${position}</td>
+            <td>${squad.name}</td>
+            <td>${shirtnum}</td>
+            <td>${squad.nationality}</td>
+            <td>${borndate.getFullYear()+"-"+month+"-"+borndate.getDate()}</td>
+        </tr>
+        `;
+  });
+
+  var articleHTML = `
+  <div class="container" id="content">
+    <div class="collection center white-text orange darken-1">
+      <h5>Team Details</h5>
+    </div>
+    <div class="card mt-2 p2">
+      <div class="row">
+        <div class="col s12 m4 center">
+          <img src="https://crests.football-data.org/${data.id}.svg" alt="${data.name}" class="responsive-img">
+        </div>
+        <div class="col s12 m8">
+          <table class="striped highlight">
+            <tbody>
+              <tr>
+                <th>Team Name</th>
+                <td>${data.name}</td>
+              </tr>
+              <tr>
+                <th>Short Name</th>
+                <td>${data.shortName}</td>
+              </tr>
+              <tr>
+                <th>Colors</th>
+                <td>${data.clubColors}</td>
+              </tr>
+              <tr>
+                <th>Stadium</th>
+                <td>${data.venue}</td>
+              </tr>
+              <tr>
+                <th>Email</th>
+                <td>${data.email}</td>
+              </tr>
+              <tr>
+                <th>Country</th>
+                <td>${data.area.name}</td>
+              </tr>
+              <tr>
+                <th>Founded</th>
+                <td>${data.email}</td>
+              </tr>
+              <tr>
+                <th>Address</th>
+                <td>${data.address}</td>
+              </tr>
+              <tr>
+                <th>Phone</th>
+                <td>${data.phone}</td>
+              </tr>
+              <tr>
+                <th>Website</th>
+                <td><a target="_blank" href="${data.website}">${data.website}</a> </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="collection center white-text orange darken-1">
+          <h5>Squads</h5>
+        </div>
+        <div>
+          <table class="striped highlight responsive-table">
+            <thead>
+              <tr>
+                <th>Position</th>
+                <th>Name</th>
+                <th>Shirt Number</th>
+                <th>Nationality</th>
+                <th>Date of Birth</th>
+              </tr>
+            </thead>
+            <tbody id="squadContainer">
+              ${squads}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  return articleHTML
 }
--- service-worker.js
+++ service-worker.js
@@ -1,159 +1,77 @@
-const CACHE_NAME = "DiBola v0.8";
-var urlsToCache = [
-  "/",
-  "/manifest.json",
-  "/nav.html",
-  "/index.html",
-  "/team.html",
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
 
-  "/assets/about.jpg",
-  "/assets/background1.jpg",
-  "/assets/comingsoon.jpg",
-  "/assets/contact.jpg",
-  "/assets/logo.png",
-  "/assets/soccer-ball.png",
-  "/assets/football-field.png",
-  "/assets/tshirt.png",
+// asset appshell
+workbox.precaching.precacheAndRoute([
+    { url: './index.html', revision: '1' },
+    { url: './manifest.json', revision: '1' },
+    { url: './nav.html', revision: '1' },
+    { url: './team.html', revision: '1' },
+    { url: './css/materialize.css', revision: '1' },
+    { url: './css/style.css', revision: '1' },
+    { url: './js/init.js', revision: '1' },
+    { url: './js/materialize.js', revision: '1' },
+    { url: './js/nav.js', revision: '1' },
+    { url: './js/api.js', revision: '1' },
+    { url: './js/db.js', revision: '1' },
+    { url: './js/idb.js', revision: '1' },
+    { url: './js/standings.js', revision: '1' },
+    { url: './js/teams.js', revision: '1' },
+    { url: './js/service-register.js', revision: '1' }
+], {
+    // Ignore all URL parameters.
+    ignoreURLParametersMatching: [/.*/]
+});
 
+// img cache
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg|PNG|)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'images',
+    }),
+)
 
-  "/css/materialize.css",
-  "/css/materialize.min.css",
-  "/css/style.css",
-
-  "/js/init.js",
-  "/js/materialize.js",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/api.js",
-  "/js/db.js",
-  "/js/idb.js",
-  "/js/standings.js",
-  "/js/matches.js",
-  "/js/teams.js",
-  "/js/service-register.js",
-
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/contact.html",
-  "/pages/league.html",
-  "/pages/matches.html",
-  "/pages/saved.html",
-
-
-
-  "/images/icons/android-icon-36x36.png",
-  "/images/icons/android-icon-48x48.png",
-  "/images/icons/android-icon-72x72.png",
-  "/images/icons/android-icon-96x96.png",
-  "/images/icons/android-icon-144x144.png",
-  "/images/icons/android-icon-192x192.png",
-
-  "/images/icons/apple-icon.png",
-  "/images/icons/apple-icon-57x57.png",
-  "/images/icons/apple-icon-60x60.png",
-  "/images/icons/apple-icon-72x72.png",
-  "/images/icons/apple-icon-76x76.png",
-  "/images/icons/apple-icon-114x114.png",
-  "/images/icons/apple-icon-120x120.png",
-  "/images/icons/apple-icon-144x144.png",
-  "/images/icons/apple-icon-152x152.png",
-  "/images/icons/apple-icon-180x180.png",
-  "/images/icons/apple-icon-precomposed.png",
-
-  "/images/icons/favicon.ico",
-  "/images/icons/favicon-16x16.png",
-  "/images/icons/favicon-32x32.png",
-  "/images/icons/favicon-96x96.png",
-  "/images/icons/favicon-512x512.png",
-
-  "/images/icons/ms-icon-70x70.png",
-  "/images/icons/ms-icon-144x144.png",
-  "/images/icons/ms-icon-150x150.png",
-  "/images/icons/ms-icon-310x310.png",
-
-  "/images/loading/loading.gif",
-
-
-  "images/logo_league/championsleague.png",
-  "images/logo_league/portugueseprimeradivision.png",
-  "images/logo_league/logo_premier_league.png",
-  "images/logo_league/logo_eredivisie.png",
-  "images/logo_league/logo_bundesliga.png",
-  "images/logo_league/logo_ligue_1.png",
-  "images/logo_league/logo_serie_a.png",
-  "images/logo_league/logo_laliga.png",
-  "images/logo_league/championship_england.png",
-  "images/logo_league/logo_brazil_serie_a.png",
-  "images/logo_league/logo_wc_2018.png",
-  "images/logo_league/europe_championship.svg",
-
-  "/images/showcase/1.jpg",
-  "/images/showcase/2.jpg",
-  "/images/showcase/3.jpg",
-  "/images/showcase/4.jpg",
-  "/images/showcase/5.jpg",
-  "/images/showcase/6.jpg",
-
-  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.googleapis.com/css2?family=Oswald&display=swap",
-  "https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs1_FvsUZiZQ.woff2",
-
-  "https://code.jquery.com/jquery-2.1.1.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0"
-];
-
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
+// caching page
+workbox.routing.registerRoute(
+    new RegExp('./pages/'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'dynamic-pages'
     })
-  );
-});
+);
 
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://api.football-data.org/v2";
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, {
-        ignoreSearch: true
-      }).then(function(response) {
-        return response || fetch(event.request);
-      })
-    )
-  }
-});
-
-
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
+// api
+var base_url = 'https://api.football-data.org/v2';
+workbox.routing.registerRoute(
+    new RegExp(base_url),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'api-cache'
     })
-  );
-});
+);
 
+//Google fonts
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://fonts.googleapis.com',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets'
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://fonts.gstatic.com',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts'
+  })
+);
+
+//Font-awesome
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'font-awesome-fonts-webfonts'
+  })
+);
+
+
+//Notification
 self.addEventListener('push', function(event) {
   var body;
   if (event.data) {
@@ -163,7 +81,7 @@ self.addEventListener('push', function(event) {
   }
   var options = {
     body: body,
-    icon: '/images/icons/favicon-96x96.png',
+    icon: './images/icons/favicon-96x96.png',
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
